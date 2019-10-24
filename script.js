5d87c5222da1b44e5dcd3fd8b6307c197a83fc98
colin <cjsords@gmail.com>
2019-10-24 15:49:10 -0400

    added extra methods

--- script.js
+++ script.js
@@ -15,6 +15,64 @@ class AddressBook {
     this.contacts.splice(0, this.contacts.length);
     console.log("Address book cleared");
   }
+  findByName(filterName) {
+    // return this.contacts.find(contact => filterName === contact.name);
+    for (const contact of this.contacts) {
+      if (filterName === contact.name) {
+        return contact;
+      }
+    }
+    // for (let i = 0; i < this.contacts.length; i++) {
+    //   if (filterName === this.contacts[i].name) {
+    //     return this.contacts[i];
+    //   }
+    // }
+  }
+  filterByRelation(filterRelation) {
+    // for (let i = 0; i < this.contacts.length; i++) {
+    //   if (filterRelation === this.contacts[i].relation) {
+    //     return this.contacts[i];
+    //   }
+    // }
+    for (const contact of this.contacts) {
+      if (filterRelation === contact.relation) {
+        return contact;
+      }
+    }
+  }
+  listNames() {
+    let justNames = [];
+    // for (let i = 0; i < this.contacts.length; i++) {
+    //   justNames.push(this.contacts[i].name);
+    // }
+    for (const contact of this.contacts) {
+      justNames.push(contact.name);
+    }
+    return justNames;
+  }
+  printNames() {
+    for (const contact of this.contacts) {
+      console.log(contact.name);
+    }
+  }
+  edit(oldName, name, email, phone, relation) {
+    for (const contact of this.contacts) {
+      if (contact.name === oldName) {
+        this.contacts.splice(
+          this.contacts.indexOf(contact),
+          1,
+          new Contact(name, email, phone, relation)
+        );
+      }
+    }
+  }
+  deleteByName(deleteName) {
+    for (const contact of this.contacts) {
+      if (contact.name === deleteName) {
+        this.contacts.splice(this.contacts.indexOf(contact), 1);
+      }
+    }
+  }
 }
 
 class Contact {
@@ -48,4 +106,21 @@ addressBook.deleteAt(2);
 // 6. Call the print function.
 print(addressBook);
 
-// addressBook.clear();
+//calling extra methods
+console.log("---");
+console.log("filter by name: ", addressBook.findByName("Dad"));
+console.log("---");
+console.log("filter by relation: ", addressBook.filterByRelation("Mother"));
+console.log("---");
+console.log(addressBook.listNames());
+console.log("---printNames");
+addressBook.printNames();
+console.log("---edit");
+addressBook.edit("Mom", "New Mom", "mom@gmail.com", "555-555-5555", "New Mom");
+print(addressBook);
+console.log("---deleteByName");
+addressBook.deleteByName("Milo");
+print(addressBook);
+console.log("---clear");
+addressBook.clear();
+print(addressBook);
