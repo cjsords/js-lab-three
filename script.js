ce049182a0c780dd28fbdfc193529ecd7d81ab80
colin <cjsords@gmail.com>
2019-10-29 16:18:54 -0400

    added part 3 code

--- script.js
+++ script.js
@@ -107,20 +107,78 @@ addressBook.deleteAt(2);
 print(addressBook);
 
 //calling extra methods
-console.log("---");
-console.log("filter by name: ", addressBook.findByName("Dad"));
-console.log("---");
-console.log("filter by relation: ", addressBook.filterByRelation("Mother"));
-console.log("---");
-console.log(addressBook.listNames());
-console.log("---printNames");
-addressBook.printNames();
-console.log("---edit");
-addressBook.edit("Mom", "New Mom", "mom@gmail.com", "555-555-5555", "New Mom");
-print(addressBook);
-console.log("---deleteByName");
-addressBook.deleteByName("Milo");
-print(addressBook);
-console.log("---clear");
-addressBook.clear();
-print(addressBook);
+// console.log("---");
+// console.log("filter by name: ", addressBook.findByName("Dad"));
+// console.log("---");
+// console.log("filter by relation: ", addressBook.filterByRelation("Mother"));
+// console.log("---");
+// console.log(addressBook.listNames());
+// console.log("---printNames");
+// addressBook.printNames();
+// console.log("---edit");
+// addressBook.edit("Mom", "New Mom", "mom@gmail.com", "555-555-5555", "New Mom");
+// print(addressBook);
+// console.log("---deleteByName");
+// addressBook.deleteByName("Milo");
+// print(addressBook);
+// console.log("---clear");
+// addressBook.clear();
+// print(addressBook);
+
+const form = document.querySelector(".form");
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  addressBook.add(
+    formData.get("name"),
+    formData.get("email"),
+    formData.get("phone"),
+    formData.get("relation")
+  );
+  display();
+  form.reset();
+});
+
+function display() {
+  //set existing list to empty string innerhtml
+  document.querySelector(".contact-display").innerHTML = "";
+  const div = document.createElement("div");
+  div.classList.add(".contact-display");
+  addressBook.contacts.forEach((contact, index) => {
+    // for (const contact of addressBook.contacts) {
+    let contactCard = document.createElement("div");
+    let contactRecordName = document.createElement("div");
+    contactRecordName.innerText = `Name: ${contact.name}`;
+    contactCard.append(contactRecordName);
+    let contactRecordEmail = document.createElement("div");
+    contactRecordEmail.innerText = `Email: ${contact.email}`;
+    contactCard.append(contactRecordEmail);
+    let contactRecordPhone = document.createElement("div");
+    contactRecordPhone.innerText = `Phone: ${contact.phone}`;
+    contactCard.append(contactRecordPhone);
+    let contactRecordRelation = document.createElement("div");
+    contactRecordRelation.innerText = `Relation: ${contact.relation}`;
+    contactCard.append(contactRecordRelation);
+    //
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+    deleteButton.classList.add("delete-button");
+    deleteButton.setAttribute("index", index);
+    contactCard.append(deleteButton);
+    //
+    contactCard.classList.add("contact-card");
+    document.querySelector(".contact-display").append(contactCard);
+  });
+}
+
+document
+  .querySelector(".contact-display")
+  .addEventListener("click", deleteRecord);
+function deleteRecord(event) {
+  if (event.target.classList.contains("delete-button")) {
+    addressBook.deleteAt(event.target.index);
+    display();
+  }
+}
+
+display();
